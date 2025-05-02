7ca19aa89c3faf76da91e62532a8c5bc5a90badc
tettuan <tettuan@gmail.com>
2025-05-02 20:26:06 +0900

    docs: update error messages to recommend using relative paths - Add recommendation to use relative paths in permission error messages - Update path validation error messages - Update prompt manager error messages

--- src/errors/permission_errors.ts
+++ src/errors/permission_errors.ts
@@ -6,23 +6,23 @@
  */
 export enum PermissionErrorMessages {
   /** Error message when read permission is denied for a file */
-  READ_FILE = "Read permission denied: Cannot read file - Please use relative paths instead of absolute paths",
+  READ_FILE = "Read permission denied: Cannot read file",
 
   /** Error message when write permission is denied for a file */
-  WRITE_FILE = "Write permission denied: Cannot write to file - Please use relative paths instead of absolute paths",
+  WRITE_FILE = "Write permission denied: Cannot write to file",
 
   /** Error message when execute permission is denied for a file */
-  EXECUTE_FILE = "Execute permission denied: Cannot execute file - Please use relative paths instead of absolute paths",
+  EXECUTE_FILE = "Execute permission denied: Cannot execute file",
 
   /** Error message when read permission is denied for a template file */
-  READ_TEMPLATE = "Read permission denied: Cannot read template file - Please use relative paths instead of absolute paths",
+  READ_TEMPLATE = "Read permission denied: Cannot read template file",
 
   /** Error message when write permission is denied for a template file */
-  WRITE_TEMPLATE = "Write permission denied: Cannot write template file - Please use relative paths instead of absolute paths",
+  WRITE_TEMPLATE = "Write permission denied: Cannot write template file",
 
   /** Error message when access permission is denied for a file */
-  ACCESS_FILE = "Access permission denied: Cannot access file - Please use relative paths instead of absolute paths",
+  ACCESS_FILE = "Access permission denied: Cannot access file",
 
   /** Error message when directory access permission is denied */
-  ACCESS_DIRECTORY = "Access permission denied: Cannot access directory - Please use relative paths instead of absolute paths",
+  ACCESS_DIRECTORY = "Access permission denied: Cannot access directory",
 }
