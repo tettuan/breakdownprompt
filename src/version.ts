cc5315d7fc7f3c543ef836ffdd0e35bfc643840a
tettuan <tettuan@gmail.com>
2026-02-17 02:32:31 +0900

    chore: bump version to 1.2.8

--- src/version.ts
+++ src/version.ts
@@ -10,7 +10,7 @@
  * across different execution contexts, including tests and subprocesses.
  * @constant
  */
-export const VERSION = "1.2.7";
+export const VERSION = "1.2.8";
 
 /**
  * Metadata about the package.
